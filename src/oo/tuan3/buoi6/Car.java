c5bf77a33b0dd9d68d97d4912156dcc0ba118351
lhphuoc2311 <phuoc2311@gmail.com>
2022-02-23 08:26:10 +0700

    source code Car class

---
+++ src/oo/tuan3/buoi6/Car.java
@@ -0,0 +1,44 @@
+/*
+*  created date: Feb 23, 2022
+*  author: cgm
+*/
+package oo.tuan3.buoi6;
+
+public class Car {
+
+    //implementation
+    //protected
+    private String carColor;
+    private double carPrice;
+
+
+    //interface
+    /**
+     * @return the carColor
+     */
+    public String getCarColor() {
+        return carColor;
+    }
+
+    /**
+     * @param carColor the carColor to set
+     */
+    public void setCarColor(String carColor) {
+        this.carColor = carColor;
+    }
+
+    /**
+     * @return the carPrice
+     */
+    public double getCarPrice() {
+        return carPrice;
+    }
+
+    /**
+     * @param carPrice the carPrice to set
+     */
+    public void setCarPrice(double carPrice) {
+        this.carPrice = carPrice;
+    }
+    
+}
